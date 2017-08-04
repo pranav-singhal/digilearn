77e15090192474f0a90e96b3c57241b28a204ba4
Thebaniyaproject <railsprojectharshit@gmail.com>
2017-08-04 22:22:25 +0530

    done and done

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( home.css )
 Rails.application.config.assets.precompile += %w( scaffolds.css )
 Rails.application.config.assets.precompile += %w( courses.scss )
+Rails.application.config.assets.precompile += %w( seller_profiles.css )
+Rails.application.config.assets.precompile += %w( books.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
